b647e2c7566a34e74c91ebc90d5fc6870e92180a
Radek Potyka <radoslaw.potyka@smcebi.edu.pl>
2017-12-23 14:54:33 +0100

    Remove tasks from ChartDisplayController initialisation. Implement method for creating demo chart. Add properties with chart resources to the controller.

--- Visualiser/modules/charts/controllers.py
+++ Visualiser/modules/charts/controllers.py
@@ -1,53 +1,30 @@
 from .services import BokehService, ChartService
-from .models import Chart
-
-
-class ChartBaseEditorController(object):
-    """
-    Controller responsible for base steps of creation process.
-
-    Args:
-                request(request): request used for chart creation.
-
-    Attributes:
-        __request(request): request used for creation of the chart.
-        __form(wtforms.Form): Current form instance to gather data.
-        __chart(Chart): Chart instance awaiting creation.
-    """
-
-    __request = None
-    __form = None
-    __chart = None
-
-    def __init__(self, request):
-        self.__request = request
-
-    @property
-    def form(self):
-        return self.__form
-
-    @form.setter
-    def form(self, form):
-        self.__form = form
-
-    @property
-    def chart(self):
-        return self.__chart
-
-    @chart.setter
-    def chart(self, chart):
-        self.__chart = chart
 
 
 class ChartDisplayController(object):
     """
     Controller responsible for handling chart creation. Contains all methods necessary for creating and storing
     charts with options provided by form data and pandas data frame object with values loaded from uploaded file.
+
+    Attributes:
+        __bokeh_resources(TemplateResourcesData): standard css and js resources for bokeh charts.
+        __chart_resources(TemplateResourcesData): js and html resources for particular bokeh charts.
     """
 
-    def __init__(self, chart_id=None):
-        chart = ChartService.get_chart(chart_id=chart_id)
+    __chart_resources = None
+    __bokeh_resources = None
+
+    def load_demo_plot(self):
+        chart = ChartService.get_demo_chart()
         plot = BokehService.generate_plot(chart)
 
-        self.bokeh_resources = BokehService.get_bokeh_resources()
-        self.chart_resources = BokehService.export_plot(plot)
+        self.__bokeh_resources = BokehService.get_bokeh_resources()
+        self.__chart_resources = BokehService.export_plot(plot)
+
+    @property
+    def chart_resources(self):
+        return self.__chart_resources
+
+    @property
+    def bokeh_resources(self):
+        return self.__bokeh_resources

32ee5b2f759ab6ea71838a8272cfeeac701ae54b
Radek Potyka <radoslaw.potyka@smcebi.edu.pl>
2018-01-20 18:17:20 +0100

    Implement base, csv and json file handler. Implement FileHandlers enum for easier export of handlers.

---
+++ Visualiser/modules/common/fileio.py
@@ -0,0 +1,90 @@
+import pandas as pd
+
+from abc import ABCMeta, abstractmethod
+from enum import Enum
+
+from .models import DataSource
+
+
+class FileHandler(metaclass=ABCMeta):
+    """
+    Base interface for file handlers. Allows for parsing file and reading columns from them.
+    """
+
+    @abstractmethod
+    def read_file(self, data_source: DataSource, file_path: str) -> pd.DataFrame:
+        """
+        Read data from a file and return pandas DataFrame object instance with parsed values.
+
+        :param data_source: (DataSource) DataSource object instance containing options for a file to read data from.
+        :param file_path: (str) Path to a file to read data from
+        :return: (pd.DataFrame) pandas DataFrame object instance with values parsed from a file.
+        """
+        pass
+
+    @abstractmethod
+    def read_columns(self, data_source: DataSource, file_path: str) -> [str]:
+        """
+        Read columns from a file and return list of those columns.
+
+        :param data_source: (DataSource) DataSource object instance containing options for a file to columns data from.
+        :param file_path: (pd.DataFrame) pandas DataFrame object instance with columns parsed from a file.
+        :return: [str] list of columns from a file.
+        """
+        pass
+
+
+class JSON(FileHandler):
+    """
+    FileHandler object for reading json type files.
+    """
+    def read_columns(self, data_source: DataSource, file_path: str) -> [str]:
+        super().read_columns(data_source, file_path)
+        data_frame = self.read_file(data_source=data_source, file_path=file_path)
+
+        return data_frame.columns.values.tolist()
+
+    def read_file(self, data_source: DataSource, file_path: str) -> pd.DataFrame:
+        super().read_file(data_source, file_path)
+
+        name = file_path + data_source.file_name
+        data_frame = pd.read_json(name,
+                                  typ='frame',
+                                  convert_dates=data_source.datetime_columns)
+        return data_frame
+
+
+class CSV(FileHandler):
+    """
+    FileHandler object for reading csv type files.
+    """
+
+    def read_file(self, data_source: DataSource, file_path: str) -> pd.DataFrame:
+        super().read_file(data_source=data_source, file_path=file_path)
+        name = file_path + data_source.file_name
+
+        df = pd.read_csv(name,
+                         skiprows=data_source.column_index,
+                         sep=data_source.separator,
+                         parse_dates=data_source.datetime_columns)
+        return df
+
+    def read_columns(self, data_source: DataSource, file_path: str) -> [str]:
+        super().read_columns(data_source=data_source, file_path=file_path)
+        name = file_path + data_source.file_name
+
+        data_frame = pd.read_csv(name,
+                                 skiprows=data_source.column_index,
+                                 sep=data_source.separator,
+                                 nrows=1)
+        return data_frame.columns.values.tolist()
+
+
+class FileHandlers(Enum):
+    """
+    Enum class containing supported FileHandlers.
+    """
+    csv = CSV
+    txt = CSV
+    dat = CSV
+    json = JSON

2aad6898f6f9cae49779947b6b796b9d45c71bdc
Radek Potyka <radoslaw.potyka@smcebi.edu.pl>
2018-02-07 15:46:48 +0100

    Add route for chart editing and display. Record app config when registering charts blueprint.

--- Visualiser/modules/charts/blueprint.py
+++ Visualiser/modules/charts/blueprint.py
@@ -1,11 +1,17 @@
 from flask import render_template, request
 
-from Visualiser.modules.charts.controllers import ChartDisplayController
+from .controllers import ChartDisplayController, ChartCreateController, ChartEditController
 
 from flask import Blueprint
 
-
 charts = Blueprint('charts', __name__)
+charts.config = {}
+
+
+@charts.record
+def record_config(setup_state):
+    config = setup_state.app.config
+    charts.config = config
 
 
 @charts.route('/home')
@@ -19,3 +25,17 @@ def preview():
     controller.load_demo_plot()
 
     return render_template("charts/preview.html", controller=controller)
+
+
+@charts.route('/create', methods=['GET', 'POST'])
+def create():
+    controller = ChartCreateController(template_path="charts/display.html")
+    return controller.activate(is_empty=False)
+
+
+@charts.route('/edit', methods=['GET', 'POST'])
+def edit():
+    is_empty = request.method != 'POST'
+    controller = ChartEditController(template_path="charts/edit.html")
+
+    return controller.activate(is_empty=is_empty)

bd5e78abde869665e16d5717cfe22a66076224b5
Radek Potyka <radoslaw.potyka@smcebi.edu.pl>
2018-02-11 22:47:38 +0100

    Provide docstrings for map module controllers. Fix map states switching. Add on error occurred method.

--- app/Visualiser/modules/maps/controllers.py
+++ app/Visualiser/modules/maps/controllers.py
@@ -4,6 +4,7 @@ from .services import MapsServiceProvider as Services
 from .forms import MapsFormHandler
 from .models import MapDocument
 from ..common.controllers import DocumentBaseEditController
+from ..common.errors import VisualiserError
 
 
 class MapBaseEditController(DocumentBaseEditController):
@@ -23,7 +24,7 @@ class MapBaseEditController(DocumentBaseEditController):
         """
         Instantiates and returns form handler based on data provided by services assigned to the controller.
 
-        :return: (FormHandler) FormHandler instance with data applied from the services.
+        :return: (MapsFormHandler) MapsFormHandler instance with data applied from the services.
         """
         return self._form_creator(columns=self._working_context_service.get_file_columns(),
                                   shapes=self._document_service.get_supported_shapes(),
@@ -35,6 +36,10 @@ class MapBaseEditController(DocumentBaseEditController):
 class MapEditController(MapBaseEditController):
     """
     Controller for handling Map edition and Map form handling.
+
+    Args:
+        map_service: (MapService) charts specific document service.
+        form_creator: (MapsFormHandler) charts specific form handler.
     """
 
     def __init__(self,
@@ -45,14 +50,29 @@ class MapEditController(MapBaseEditController):
 
     def on_form_submitted(self, is_valid: bool = False):
         if is_valid:
-            return self._router_state_service.submit('maps.create')
+            return self._router_state_service.submit('.create')
         return self._router_state_service.render(template_path=self._template_path, controller=self)
 
     def on_document_disposed(self):
-        return self._router_state_service.go('maps.index')
+        return self._router_state_service.go('.index')
 
 
 class MapCreateController(MapBaseEditController):
+    """
+    Controller handling creation of the chart by parsing data from form into map document object and creating it
+    using provided document_create_service.
+
+    Args:
+        map_service: (MapService) maps specific document service.
+        form_creator: (MapsFormHandler) maps specific form handler.
+        document_create_service (FoliumService) folium service for creating and exporting plots.
+
+    Attributes:
+        @property map_options: (MapOptions) options for a chart parsed from controllers form.
+        @property map_resources: (str) template resources for bokeh library.
+        _document_creator_service: (FoliumService) bokeh service for creating and exporting plots.
+    """
+
     __map_resources = None
     __map_options = None
 
@@ -66,13 +86,18 @@ class MapCreateController(MapBaseEditController):
 
     def load_map(self, map_document: MapDocument) -> None:
         """
+        Generates and exports map from map document object and stores resources for its display.
 
-        :param map_document:
+        :param map_document: (MapDocument) MapDocument object instance with object and data source configuration for
+        a map.
         :return: None
         """
         map_object = self._document_creator_service.generate_map(map_document=map_document)
-        path_to_map = self._document_creator_service.export_map(map_object=map_object)
-        self.__map_resources = path_to_map
+        map_resources = self._document_creator_service.export_map(map_object=map_object)
+        self.__map_resources = map_resources
+
+    def on_error_occurred(self, error: VisualiserError, next_state: str = "maps.edit"):
+        return super().on_error_occurred(error=error, next_state=next_state)
 
     def setup_data_source(self) -> pd.DataFrame:
         creator = self._get_form_creator()
@@ -93,12 +118,13 @@ class MapCreateController(MapBaseEditController):
         self.__map_options = map_document.model
 
         map_document.data_source = self.setup_data_source()
+
         self.load_map(map_document=map_document)
 
         return self._router_state_service.render(template_path=self._template_path, controller=self)
 
     def on_document_edited(self):
-        return self._router_state_service.submit('maps.edit')
+        return self._router_state_service.submit('.edit')
 
     @property
     def map_options(self):
@@ -110,4 +136,4 @@ class MapCreateController(MapBaseEditController):
 
     def on_document_disposed(self):
         super().on_document_disposed()
-        return self._router_state_service.go('maps.index')
+        return self._router_state_service.go('.index')
